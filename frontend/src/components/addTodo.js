66a1af966749afdfba97631c244ade40d3ce9a94
jessicaniemi <jessica.nordin@klarna.com>
2018-09-21 17:25:13 +0200

    before weekend commit

--- frontend/src/components/addTodo.js
+++ frontend/src/components/addTodo.js
@@ -1,17 +1,30 @@
-import React, { Component } from 'react';
+import React  from 'react';
+import styled from 'styled-components';
+
+const FormContainer = styled.fieldset`
+  padding: 5px;
+  margin: 10px;
+  border: 1px solid black;
+  max-width: 80vw;
+  display: flex;
+  justify-content: center; 
+`;
+const Input = styled.input`
+  margin: 0.5em;
+`;
 
 const AddTodo = () => {
   return(
-    <div>
+    <FormContainer>
+      <legend>Add todo</legend>
       <form>
-        <label>Add todo</label>
         <div>
-            <input placeholder="Add Task"></input>
-          <input placeholder="Todo Date"></input>
-            <button type="submit">Add</button>
+          <Input placeholder="Add Task"></Input>
+          <Input placeholder="Todo Date" type="date"></Input>
+          <button type="submit">Add</button>
         </div>
       </form>
-    </div>
+    </FormContainer>
   )
 };
 

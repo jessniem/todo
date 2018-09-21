ad7ec0dac6232f4524fb2c6fd267c0147fc6b858
jessicaniemi <jessica.nordin@klarna.com>
2018-09-21 08:13:38 +0200

    added update functionality for todo title

--- server/controllers/todos.js
+++ server/controllers/todos.js
@@ -44,5 +44,26 @@ module.exports = {
         return res.status(200).send(todo);
       })
       .catch(error => res.status(400).send(error));
-  }
+  },
+  update(req, res) {
+    return Todo
+      .findById(req.params.todoId, {
+        include: [{
+          model: TodoItem,
+          as: 'todoItems'
+        }],
+      })
+      .then(todo => {
+        if(!todo) {
+          return res.status(404).send({
+            message: 'Todo not found',
+          });
+        }
+        return todo.update({
+          title: req.body.title || todo.title,
+        })
+        .then(() => res.status(200).send(todo))
+          .catch((error) => res.status(400).send(error));
+      })
+  },
 };

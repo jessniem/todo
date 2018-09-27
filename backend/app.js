57d9b40d36ab4adf433161b1b976135e6a8d21de
jessicaniemi <jessica.nordin@klarna.com>
2018-09-27 12:49:02 +0200

    fixed CORS issue, can get data from db

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const app = express();
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-
+app.use(cors());
 require('./server/routes')(app);
 
 // Setup a default catch-all route that sends back message in JSON format.
